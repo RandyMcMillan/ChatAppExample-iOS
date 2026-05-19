aa654ec4af14a0dbf63fab1e970f56bf8b19b9b9
randymcmillan <randymcmillan@protonmail.com>
2026-05-19 12:11:50 -0400

    redis:remove

--- redis/Sources/Redis/Exports.swift
+++
@@ -1,4 +0,0 @@
-@_documentation(visibility: internal) @_exported import RediStack
-@_documentation(visibility: internal) @_exported import struct Foundation.URL
-@_documentation(visibility: internal) @_exported import struct Logging.Logger
-@_documentation(visibility: internal) @_exported import struct NIO.TimeAmount
