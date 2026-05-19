9f4cb8c06ccd342259731752aab5e4ba3830bc04
randymcmillan <randymcmillan@protonmail.com>
2026-05-19 14:43:11 -0400

    swift-libp2p-pubsub/Sources/LibP2PPubSub/Routers/Floodsub/Application+Floodsub.swift

--- swift-libp2p-pubsub/Sources/LibP2PPubSub/Routers/Floodsub/Application+Floodsub.swift
+++ swift-libp2p-pubsub/Sources/LibP2PPubSub/Routers/Floodsub/Application+Floodsub.swift
@@ -18,7 +18,7 @@ extension Application.PubSubServices.Provider {
     public static var floodsub: Self {
         .init {
             $0.pubsub.use { app -> FloodSub in
-                let fsub = try! FloodSub(group: app.eventLoopGroup, libp2p: app)
+                let fsub = try! FloodSub(group: app.eventLoopGroup, libp2p: app, emitSelf: true)
                 app.lifecycle.use(fsub)
                 return fsub
             }
