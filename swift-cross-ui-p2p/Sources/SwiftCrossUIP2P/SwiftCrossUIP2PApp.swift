21d198634982695476f89b84601364d70da4a516
randymcmillan <randymcmillan@protonmail.com>
2026-05-21 19:55:59 -0400

    swift-cross-ui-p2p/Sources/SwiftCrossUIP2P/SwiftCrossUIP2PApp.swift

--- swift-cross-ui-p2p/Sources/SwiftCrossUIP2P/SwiftCrossUIP2PApp.swift
+++ swift-cross-ui-p2p/Sources/SwiftCrossUIP2P/SwiftCrossUIP2PApp.swift
@@ -9,7 +9,7 @@ import LibP2PNoise
 import LibP2PYAMUX
 import SwiftCrossUI
 
-#if os(iOS) || targetEnvironment(macCatalyst)
+#if os(iOS)
     import GnostrGit
     import XGit
 #endif
@@ -260,7 +260,7 @@ final class P2PDemoViewModel {
         let selectedCommitOID = gitSelectedCommitOID
         gitIsLoading = true
         gitLastError = nil
-        #if os(iOS) || targetEnvironment(macCatalyst)
+        #if os(iOS)
             gitRefreshTask = Task.detached(priority: .background) { [path, selectedCommitOID] in
                 let snapshot = GitRepoSnapshotLoader.loadGitSnapshot(
                     path: path,
@@ -443,7 +443,7 @@ enum GitRepoSnapshotLoader {
         return formatter
     }()
 
-    #if os(iOS) || targetEnvironment(macCatalyst)
+    #if os(iOS)
     static func loadGitSnapshot(path: String, selectedCommitOID: String?) -> GitRepoSnapshot {
         let url = URL(fileURLWithPath: path, isDirectory: true)
         let credentialsURL = FileManager.default.temporaryDirectory
