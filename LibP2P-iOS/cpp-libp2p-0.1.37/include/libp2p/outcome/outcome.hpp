22a92c2c6b7602a486dbc70adc6138231cf54bcd
randymcmillan <randymcmillan@protonmail.com>
2026-05-20 21:06:47 -0400

    LibP2P-iOS/cpp-libp2p-0.1.37/include/libp2p/outcome/outcome.hpp

--- LibP2P-iOS/cpp-libp2p-0.1.37/include/libp2p/outcome/outcome.hpp
+++ LibP2P-iOS/cpp-libp2p-0.1.37/include/libp2p/outcome/outcome.hpp
@@ -9,6 +9,8 @@
 #include <boost/outcome/result.hpp>
 
 #include <system_error>
+#include <type_traits>
+#include <typeinfo>
 #include <utility>
 
 #define LIBP2P_OUTCOME_JOIN2(a, b) a##b
@@ -29,6 +31,50 @@
 #define _OUTCOME_OVERLOAD(_1, _2, s, ...) _OUTCOME_TRY_##s
 #define OUTCOME_TRY(...) \
   _OUTCOME_OVERLOAD(__VA_ARGS__, out, void)(LIBP2P_OUTCOME_UNIQUE_NAME(outcome_res_), __VA_ARGS__)
+
+#define _LIBP2P_ERROR_CODE_MESSAGE(E, ns, e) std::string ns errorCodeMessage(E e)
+#define _LIBP2P_MAKE_ERROR_CODE(E)                                           \
+  inline std::error_code make_error_code(const E &e) {                       \
+    return {static_cast<int>(e), ::libp2p::outcome::EnumErrorCategory<E>::get()}; \
+  }
+#define _LIBP2P_ENUM_ERROR_CODE(friend_, E)           \
+  friend_ inline _LIBP2P_ERROR_CODE_MESSAGE(E, , e);  \
+  friend_ _LIBP2P_MAKE_ERROR_CODE(E)                  \
+  friend_ inline _LIBP2P_ERROR_CODE_MESSAGE(E, , e)
+#define Q_ENUM_ERROR_CODE(E) _LIBP2P_ENUM_ERROR_CODE(, E)
+#define Q_ENUM_ERROR_CODE_FRIEND(E) _LIBP2P_ENUM_ERROR_CODE(friend, E)
+#define _OUTCOME_HPP_DECLARE_ERROR(friend_, E) \
+  friend_ _LIBP2P_ERROR_CODE_MESSAGE(E, , );   \
+  friend_ _LIBP2P_MAKE_ERROR_CODE(E)
+#define OUTCOME_HPP_DECLARE_ERROR(ns, E) \
+  namespace ns {                         \
+    _OUTCOME_HPP_DECLARE_ERROR(, E)      \
+  }
+#define OUTCOME_CPP_DEFINE_CATEGORY(ns, E, e) _LIBP2P_ERROR_CODE_MESSAGE(E, ns::, e)
+
+namespace libp2p::outcome {
+  template <typename E>
+  concept IsEnumErrorCode =
+      std::is_enum_v<E> and requires { errorCodeMessage(E{}); };
+
+  template <IsEnumErrorCode E>
+  class EnumErrorCategory final : public std::error_category {
+   public:
+    const char *name() const noexcept final {
+      return typeid(E).name();
+    }
+    std::string message(int code) const final {
+      return errorCodeMessage(static_cast<E>(code));
+    }
+    static const EnumErrorCategory<E> &get() {
+      static const EnumErrorCategory<E> category;
+      return category;
+    }
+  };
+}  // namespace libp2p::outcome
+
+template <libp2p::outcome::IsEnumErrorCode E>
+struct std::is_error_code_enum<E> : std::true_type {};
 
 namespace outcome {
   template <class R>
