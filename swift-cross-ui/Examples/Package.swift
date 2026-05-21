a6b90c2c0465913b0bfa091d266d06b7e57ca747
randymcmillan <randymcmillan@protonmail.com>
2026-05-21 07:34:58 -0400

    swift-cross-ui:initial impl

---
+++ swift-cross-ui/Examples/Package.swift
@@ -0,0 +1,125 @@
+// swift-tools-version: 5.10
+
+import Foundation
+import PackageDescription
+
+var exampleDependencies: [Target.Dependency] = [
+    .product(name: "SwiftCrossUI", package: "swift-cross-ui"),
+    .product(name: "DefaultBackend", package: "swift-cross-ui"),
+]
+var hotReloadingDependencies: [Package.Dependency] = []
+
+// The Swift Bundler runtime requires Swift >=6.0
+#if compiler(>=6.0)
+    hotReloadingDependencies = [
+        .package(
+            url: "https://github.com/moreSwift/swift-bundler",
+            revision: "6527cb29084f79cb4d2300f98acd7c057abb4a67"
+        )
+    ]
+    exampleDependencies.append(
+        .product(
+            name: "SwiftBundlerRuntime",
+            package: "swift-bundler",
+            condition: .when(platforms: [.macOS, .linux])
+        )
+    )
+#endif
+
+let package = Package(
+    name: "Examples",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .macCatalyst(.v13), .visionOS(.v1)],
+    dependencies: [
+        .package(name: "swift-cross-ui", path: ".."),
+        .package(
+            url: "https://github.com/stackotter/swift-miniaudio",
+            .upToNextMinor(from: "0.1.2")
+        ),
+    ] + hotReloadingDependencies,
+    targets: [
+        .executableTarget(
+            name: "ControlsExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "CounterExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "RandomNumberGeneratorExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "WindowingExample",
+            dependencies: exampleDependencies,
+            resources: [.copy("Banner.png")]
+        ),
+        .executableTarget(
+            name: "GreetingGeneratorExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "NavigationExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "SplitExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "SpreadsheetExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "StressTestExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "NotesExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "PathsExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "WebViewExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "HoverExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "ForEachExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "AdvancedCustomizationExample",
+            dependencies: exampleDependencies,
+            resources: [.copy("Banner.png")]
+        ),
+        .executableTarget(
+            name: "ColorsExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "GradientsExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "MusicPlayerExample",
+            dependencies: [
+                .product(name: "MiniAudio", package: "swift-miniaudio")
+            ] + exampleDependencies
+        ),
+        .executableTarget(
+            name: "FontsExample",
+            dependencies: exampleDependencies
+        ),
+        .executableTarget(
+            name: "TapGesturesExample",
+            dependencies: exampleDependencies
+        )
+    ]
+)
