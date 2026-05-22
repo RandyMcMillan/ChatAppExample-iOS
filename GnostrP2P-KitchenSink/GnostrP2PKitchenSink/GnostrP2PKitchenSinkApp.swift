5eac5d60dd50474d7b5e513450a046e5f0b1d912
randymcmillan <randymcmillan@protonmail.com>
2026-05-21 20:42:02 -0400

    GnostrP2P-KitchenSink:initial impl

---
+++ GnostrP2P-KitchenSink/GnostrP2PKitchenSink/GnostrP2PKitchenSinkApp.swift
@@ -0,0 +1,16 @@
+//
+//  GnostrP2PKitchenSinkApp.swift
+//  GnostrP2P-KitchenSink
+//
+
+import SwiftUI
+
+@main
+struct GnostrP2PKitchenSinkApp: App {
+    var body: some Scene {
+        WindowGroup("GnostrP2P Kitchen Sink") {
+            ContentView()
+        }
+        .defaultSize(width: 1100, height: 780)
+    }
+}
